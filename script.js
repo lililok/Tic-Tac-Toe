b50d49e93cca70a5588712eaed70480c5ce31d13
lililok <eshanu.sofia@yandex.ru>
2024-05-18 19:21:52 +0300

    gathering game mechanics

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const gameBoard {};
+const gameBoard = {
+    currentGameBoard: []
+};
+
+const gameControl = {};
+
+const playerOne = {
+    name: "player1",
+    mark: "X",
+    score: 0
+};
+
+const playerTwo = {
+    name: "player2",
+    mark: "O",
+    score: 0
+};
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], 
+    [0, 4, 8],
+    [2, 4, 6], 
+  ];
