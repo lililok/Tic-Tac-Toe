af939ed4fb121fc7823d5b8d891c4a8f140f6d4e
lililok <eshanu.sofia@yandex.ru>
2024-05-19 10:46:04 +0300

    more logic in progress

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const gameBoard = {
-    currentGameBoard: []
+    currentGameBoard: ['', '', '', '', '', '', '', '', '']
 };
-
-const gameControl = {};
-
 const playerOne = {
     name: "player1",
     mark: "X",
-    score: 0
+    score: 0,
+    moves: []
 };
-
 const playerTwo = {
     name: "player2",
     mark: "O",
-    score: 0
+    score: 0,
+    moves: []
 };
 
-const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8], 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8], 
-    [0, 4, 8],
-    [2, 4, 6], 
-  ];
+const playGame = (function() {
+    let currentPlayer = playerOne;
+
+
+    function checkWin(moves) {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8], 
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8], 
+            [0, 4, 8],
+            [2, 4, 6], 
+        ];
+    }
+
+    function restartGame() {
+        gameBoard.currentGameBoard.fill('');
+        playerOne.moves = [];
+        playerTwo.moves = [];
+        currentPlayer = playerOne;
+    }
+
+})();
+
